caa767de8f1ec2b544867d3c71dd721119657516
HoangTien339 <tiennh21@fpt.edu.vn>
2022-05-17 14:26:24 +0700

    Buoi 4: Swing

---
+++ src/main/java/su22b1/sof3021/controllers/admin/UserController.java
@@ -0,0 +1,25 @@
+package su22b1.sof3021.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import su22b1.sof3021.beans.User;
+
+@Controller
+@RequestMapping("/admin/users")
+public class UserController {
+	@GetMapping("create")
+	public String create(@ModelAttribute("user") User user)
+	{
+		return "admin/users/create";
+	}
+	
+	@PostMapping("store")
+	public String store(@ModelAttribute("user") User user)
+	{
+		return "admin/users/create";
+	}
+}
